809bc5e41e1d7ad0e8205c7980b125c73182cfcc
gdumani <gdumani@gmail.com>
2021-11-23 15:37:53 -0600

    create BookArray with methods for localstorage

--- src/awesome.js
+++ src/awesome.js
@@ -1,2 +1,24 @@
-const title = document.createElement ('h1');
-title.appendChild('Awasome Books');
+const book = (author,title) => ({author,title});
+
+const BookArray = () => {
+  const getBooks = () => {
+    JSON.parse(localStorage.getItem(bookArray)) || [];
+  }; 
+
+  const setBooks = (book) => {
+    const allbooks = getBooks();
+    allbooks.push(book);
+    localStorage.setItem(allBooks,JSON.stringify(allBooks));
+  };
+
+  document.getElementsById('book-form').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(JSON.stringify(e));
+  });
+};
+//   const
+//   const a = document.getElementById('title').value
+//   const b = document.getElementById('author').value
+// document.getElementsById('book-list').innerHTM= a
+
+console.log('hola')
