1188d43eba7e0f473f05977b47caade563dc3af0
gdumani <gdumani@gmail.com>
2021-11-25 12:52:18 -0600

    add addBooks funtion an submit event listener

--- src/awesome.js
+++ src/awesome.js
@@ -1,33 +1,24 @@
-const book = (author,title) => ({author,title});
+let books=[]
 
-const BookArray = () => {
-  const getBooks = () => {
-    JSON.parse(localStorage.getItem('allBooks')) || [];
-  }; 
-
-  const setBooks = (book) => {
-    const allBooks = getBooks();
-    allBooks.push(book);
-    localStorage.setItem('allBooks',JSON.stringify(allBooks));
+function getBook() {
+  const book = {
+    title:  document.getElementById('title').value,
+    author: document.getElementById('author').value
   };
+  return book;
+}
 
-  // const UI = () => {
-  //    getBooks(book) {
-  //      const list = document.getElementById('book-list');
-  //      const row = document.createElement('tr');
-  //      row.innerHTML = ` 
-  //      <td>${book.title}</td>
-  //      <td>${book.author}</td>
-  //      <td><button type='button' class='delete'>Remove this book</button></td>
-  //      `;
-  //      list.appendChild(row);
-  //    } 
-  // }
+function addBook(b) {
+  const book=getBook();
+  if (JSON.stringify(b[b.length-1])!=JSON.stringify(book) &&
+    book.title!='' && book.author!=''){
+    b.push(book);
+  }  
+  localStorage.setItem('books',JSON.stringify(b));
+}
 
-  const form = document.querySelector('#book-form');
-  
-};
-document.getElementById('submit').addEventListener('submit', () => { alert('SUBMITED')});
+const submitBtn=document.getElementById('submit');
+submitBtn.addEventListener('click', () => {addBook(books)});
 
-// const author = e.target.author.value;
-// const title = e.target.title.value; 
+
+function displayBooks(){}
